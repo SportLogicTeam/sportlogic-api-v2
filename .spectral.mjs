4db81e36566844b5e5596a6daa33c21f214bc536
developer@sportlogic.com.au <developer@sportlogic.com.au>
2025-03-13 22:02:20 +1100

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2ODQy/spectral.js?branch_slug=master&token=7e556551-3531-4e35-a493-c358d7f7b8f8";
+export default { extends: ruleset };
